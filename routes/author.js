763437a88b9c4648ef2a59bee21b20f455502698
Sanmi01 <supersanmi01@gmail.com>
2021-02-08 22:45:38 +0100

    changes

--- routes/author.js
+++ routes/author.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
 const Author = require('../models/Author');
+const models = require('../models');
 
-router.get('/', (req, res) => 
-    Author.findAll()
+router.get('/', (req, res) =>
+    models.Author.findAll()
     .then(authors => {
         console.log(authors);
         res.sendStatus(200);
